adb015017e10a542064b97993a57c6006dd7e36f
Madhu890-SS <pavanvasam7@gmail.com>
2025-09-07 00:39:16 +0530

    Added Header and Footer

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,12 +1,17 @@
 
 import './App.css'
+import FooterComponent from './components/FooterComponent'
+
+import HeaderComponent from './components/HeaderComponent'
 import ListEmployee from './components/ListEmployee'
 
 function App() {
 
   return (
     <>
+    <HeaderComponent/>
      <ListEmployee/>
+    <FooterComponent/>
     </>
   )
 }
---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FooterComponent() {
+  return (
+    <div>
+        <footer className='footer'>
+           <span>All rights reserved 2025 by Pavan Vasam</span> 
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
